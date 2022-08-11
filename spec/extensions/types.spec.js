d0bc89991cab31baaa2fb4a80703c6662300be87
Shahzad Naeem <shahzad65naeem@gmail.com>
2022-08-11 10:19:08 +0100

    Add extension - might be better in primitive-data-types?

---
+++ spec/extensions/types.spec.js
@@ -0,0 +1,37 @@
+const answers = require('../../src/extensions/types.js')
+
+describe('variable types', () => {
+  it('pickMe', () => {
+    expect(answers.pickMe.toBe('Pick me!'))
+  })
+  it('imTrue', () => {
+    expect(answers.imTrue).toBe(true)
+  })
+  it('aReal', () => {
+    expect(answers.aReal).toBe(3.14)
+  })
+  it('aString', () => {
+    expect(answers.aString).toBe('hey')
+  })
+  it('anArray', () => {
+    expect(answers.anArray).toEqual([1, 2, 3])
+  })
+  it('aNumber', () => {
+    expect(answers.aNumber).toBe(-12)
+  })
+  it('anObject', () => {
+    expect(answers.anObject).toEqual({ i: 'object!' })
+  })
+  it('imFalse', () => {
+    expect(answers.imFalse).toBe(false)
+  })
+  it('imBigInt', () => {
+    expect(answers.imBigInt).toBe(1000000n)
+  })
+  it('imUndefined', () => {
+    expect(answers.imFalse).toBe(undefined)
+  })
+  it('imNull', () => {
+    expect(answers.imNull).toBe(null)
+  })
+})
---
+++ src/extensions/types.js
@@ -0,0 +1,79 @@
+//
+// Variable types
+//
+// MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#javascript_types
+// MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+//
+
+// Update the answers below the numbered questions below
+
+// An array of choices to pick answers from
+const CHOICES = [
+  'Pick me!', // choose this with pick(0)
+  null,
+  undefined, // choose this with pick(2) etc ...
+  1,
+  3.14,
+  10000000n,
+  'hey',
+  [1, 2, 3],
+  { i: 'object!' },
+  1 == 1, // HINT: Is this true or false?
+  3 == 4 // HINT: Is this true or false?
+]
+
+// Use the following function to 'pick' an answer to the questions below
+// NOTE: Answers are numbered from 0
+
+// examples
+let pickMe = pick(0) // to pick the 1st answer use 0
+pickMe = pick(3) // to pick the 4th answer use 3 etc
+
+function pick(n) {
+  return CHOICES[n]
+}
+
+// 1. Pick true using the pick function - by changing 0 to pick your answer
+const imTrue = pick(0)
+
+// 2. Pick a real number
+const aReal = pick(0)
+
+// 3. Pick a string
+const aString = pick(3)
+
+// 4. Pick an array
+const anArray = pick(1)
+
+// 5. Pick a (simple) number
+const aNumber = pick(0)
+
+// 6. Pick an object
+const anObject = pick(1)
+
+// 7. Pick false
+const imFalse = pick(0)
+
+// 8. Pick a BigInt
+const imBigInt = pick(1)
+
+// 9. Pick undefined
+const imUndefined = pick(0)
+
+// 10. Pick null
+const imNull = pick(0)
+
+// Do not edit below this line
+module.exports = {
+  pickMe,
+  imTrue,
+  aReal,
+  aString,
+  anArray,
+  aNumber,
+  anObject,
+  imFalse,
+  imBigInt,
+  imNull,
+  imUndefined
+}
