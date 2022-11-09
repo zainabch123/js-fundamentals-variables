8dda3e149828552b86f4cd629ce9bb67e27a7f9c
dearshrewdwit <dearshrewdwit@gmail.com>
2022-11-09 16:10:09 +0000

    disable eslint starting errors

--- src/declaration.js
+++ src/declaration.js
@@ -6,11 +6,13 @@
 // do not edit below this line
 let firstNameExport = ''
 try {
+  /* eslint-disable no-undef */
   firstNameExport = firstName
 } catch (e) {}
 
 let ageExport = 0
 try {
+  /* eslint-disable no-undef */
   ageExport = age
 } catch (e) {}
 
