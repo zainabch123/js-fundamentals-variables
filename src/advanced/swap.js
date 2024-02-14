8ca9ee02e2a2a3cc8252a68dd897a904f83ca2f8
Nathan King <nathanking3590@gmail.com>
2024-02-14 08:34:10 +0000

    Update swap.js

--- src/advanced/swap.js
+++ src/advanced/swap.js
@@ -1,6 +1,6 @@
 let a = 8
 let b = 10
 
-// TODO: Swap the values of a and b; extra points if you can do it without using a temporary variable
+// TODO: Swap the values of a and b without changing lines 1 and 2; extra points if you can do it without using a temporary variable
 
 module.exports = { a, b }
